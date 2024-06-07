5c89a40d1b9a362a9017917fd16c54c18feed36d
likewoody <jsungj3@gmail.com>
2024-06-07 17:26:35 +0900

    frame 입니다.

---
+++ Vinoble/TCA/Test.swift
@@ -0,0 +1,29 @@
+//
+//  Test.swift
+//  Vinoble
+//
+//  Created by Woody on 6/4/24.
+//
+
+import ComposableArchitecture
+
+@Reducer
+struct TestFeature{
+    
+    struct State{
+        
+    }
+    
+    enum Action{
+        
+    }
+    
+    var body: some ReducerOf<Self>{
+        Reduce { state, action in
+            switch action{
+                
+            }
+        }
+    }
+    
+}
---
+++ Vinoble/Vinoble/VinobleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinobleApp.swift
+//  Vinoble
+//
+//  Created by Woody on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct VinobleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
