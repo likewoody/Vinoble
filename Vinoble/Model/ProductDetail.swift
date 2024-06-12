8c96b2f74d014b33f657072a9c56aed95e2ea6a9
이대근 <gerrardlee@idaegeun-ui-noteubug.local>
2024-06-12 18:55:02 +0900

    2024-06-12 commit
    TCA 수정, 서버 연결

---
+++ Vinoble/Model/ProductDetail.swift
@@ -0,0 +1,24 @@
+//
+//  ProductDetail.swift
+//  Vinoble
+//
+//  Created by 이대근 on 6/12/24.
+//
+
+import Foundation
+
+struct DetailProduct {
+    var foodname1: String
+    var foodname2: String
+    var foodname3: String
+    var foodname4: String
+    var foodname5: String
+    
+    init(foodname1: String, foodname2: String, foodname3: String, foodname4: String, foodname5: String) {
+        self.foodname1 = foodname1
+        self.foodname2 = foodname2
+        self.foodname3 = foodname3
+        self.foodname4 = foodname4
+        self.foodname5 = foodname5
+    }
+}
